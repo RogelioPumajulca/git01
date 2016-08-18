1939bc70f8855d6f21a5ee8a74d2e9b99bc08f12
Rogelio <alropusa@gmail.com>
2016-08-18 18:17:53 -0500

    Commit 18:17

---
+++ TestAppV2/src/Pqt1/Clase1.java
@@ -0,0 +1,13 @@
+package Pqt1;
+
+public class Clase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TestAppV2 18:13");
+		System.out.println("TestAppV2 18:15");
+		System.out.println("TestAppV2 18:16");
+		System.out.println("TestAppV2 18:17");
+	}
+
+}
---
+++ TestAppV2/src/Pqt2/Clase2.java
@@ -0,0 +1,5 @@
+package Pqt2;
+
+public class Clase2 {
+
+}
